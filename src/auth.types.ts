e06e4e2d479bc5d784d7745f80e1e9786c083356
lxj <1176891456@qq.com>
2026-05-23 18:54:27 +0800

    chore(auth): install next-auth@beta and add session type augmentation

---
+++ src/auth.types.ts
@@ -0,0 +1,21 @@
+import type { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      backendAccessToken?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    backendAccessToken?: string;
+  }
+}
+
+declare module '@auth/core/jwt' {
+  interface JWT {
+    id?: string;
+    backendAccessToken?: string;
+  }
+}
